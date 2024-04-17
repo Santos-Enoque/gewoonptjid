c08be14f02e1d1683af9a9c0d57c5111e737f92c
Santos Safrao <santossafrao@Santoss-MacBook-Pro.local>
2024-04-17 09:22:05 +0900

    added avatar

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,6 +1,7 @@
 import Container from "../container";
 import Logo from "./logo";
 import Search from "./search";
+import UserMenu from "./userMenu";
 const Navbar = () => {
     return <div className="fixed w-full bg-white z-10 shadow-sm">
         <div className="py-4 border-b-[1px]">
@@ -8,6 +9,7 @@ const Navbar = () => {
                 <div className="flex  flex-row justify-between items-center gap-3 md:gap-0">
                     <Logo />
                     <Search />
+                    <UserMenu/>
                 </div>
             </Container>
         </div>
--- app/components/navbar/search.tsx
+++ app/components/navbar/search.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { BiSearch } from "react-icons/bi";
+
 const Search = () => {
     return <div className="
     border-[1px] 
@@ -52,6 +54,14 @@ const Search = () => {
                 sm:block
                 ">
                     Add guest
+                </div>
+                <div className="
+               p-2
+               bg-black 
+               text-white
+               rounded-full
+                ">
+                    <BiSearch size={18} />
 
                 </div>
             </div>
